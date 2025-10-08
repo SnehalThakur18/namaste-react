e0387cd0430ec7f71e76c26a478906bc69eec3b1
Snehal Thakur <snehal.thakur@in.ncs-i.com>
2025-10-08 15:18:21 +0530

    Functional Components

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: "parent"}, [
-  React.createElement("div", {id: "child"}, [
-    React.createElement("h1",{},"I'm an h1 tag"),
-    React.createElement("h2",{},"I'm an h2 tag"),
-  ]),
-  React.createElement("div", {id: "child2"}, [
-    React.createElement("h1",{},"I'm an h1 tag"),
-    React.createElement("h2",{},"I'm an h2 tag"),
-  ]),
-]);
+const Title = () => (
+    <h1 className="heading">Hello world from react element.</h1>
+);
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <h2>Data coming from Functional Component</h2>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
